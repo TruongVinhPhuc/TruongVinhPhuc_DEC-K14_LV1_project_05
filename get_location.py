694284b0dcb562f05b4c579e748886ac1449838f
TruongVinhPhuc <139362781+TruongVinhPhuc@users.noreply.github.com>
2025-06-03 12:52:20 +0700

    Update get_location.py

--- get_location.py
+++ get_location.py
@@ -1,7 +1,7 @@
-import pymongo
 import IP2Location
 from bson import ObjectId
-
+import pymongo
+import time
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 
 db = myclient["test"]
@@ -12,46 +12,53 @@ ip_location_collection = db["ip_location"]
 ip_location_error_collection = db["ip_location_error"]
 
 
-def get_data(last_access_id: ObjectId, limit: int):
-    return summary_collection.find({"_id": {"$gt": last_access_id}}).limit(limit)
+def get_data():
+    pipeline = [
+        { "$group": { "_id": "$ip" } }
+    ]
+    cursor = summary_collection.aggregate(pipeline, allowDiskUse=True)
+    return [doc["_id"] for doc in cursor]
 
 
 def process_ip(ip: str):
     res = ip_database.get_all(ip)
-    return res.country_long, res.region, res.city, res.latitude, res.longtitude, res.district
+    return res.country_long, res.region, res.city, res.district
 
 
 def main():
+    start = time.time()
     ip_location_collection.delete_many({})
     ip_location_error_collection.delete_many({})
-    min_id = summary_collection.find().sort({"_id": 1}).limit(1) # Find minimum _id of the collection as using find() alone doesn't guarantee the order of the documents 
-    last_access_id = min_id[0]["_id"]
-    limit = 10000
-    data = list(get_data(last_access_id, limit))
-    unique_ips = set()
+    data = get_data()
 
-    while data:
-        valid_ips = []
-        invalid_ips = []
-        for item in data:
-            # print(item["_id"])
-            ip = item["ip"]
-            if ip not in unique_ips:
-                country, region, city, latitude, longtitude, district = process_ip(ip)
-                unique_ips.add(ip)
-                valid_ips.append(
-                    {"summary_id": item["_id"], "ip": ip, "country": country, 'region': region, 'city': city, 'district': district}
-                )
+    batch = []
+    batch_len = 0
 
-        if valid_ips:
-            ip_location_collection.insert_many(valid_ips)
-        if invalid_ips:
-            ip_location_error_collection.insert_many(invalid_ips)
+    for ip in data:
+        try:
+            country_long, region, city, district = process_ip(ip)
+            batch.append({
+                "ip": ip,
+                "country_long": country_long,
+                "region": region,
+                "city": city,
+                "district": district
+            })
+            batch_len += 1
+            if batch_len == 100000:
+                ip_location_collection.insert_many(batch)
+                batch = []
+                batch_len = 0
 
-        last_access_id = data[-1]["_id"]
-        # print(last_access_id)
-        data = list(get_data(last_access_id, limit))
-        # print(len(data))
+        except Exception as e:
+            print(f"Error processing IP {ip}: {e}")
+            ip_location_error_collection.insert_one({
+                "ip": ip,
+                "error": str(e),
+            })
+    print("IP location data processing completed.")
 
+    end = time.time()
+    print(f"Time taken: {end - start} seconds")
 
 main()
